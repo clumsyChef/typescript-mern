2b7c7c59f681aa4dd64c08fb7f7dca199fd42aa8
Sarthak Jha <sarthakjha94@gmail.com>
2023-05-09 00:15:31 +0530

    added bilerplate in blog models

--- server/src/models/blogs/BlogModels.ts
+++ server/src/models/blogs/BlogModels.ts
@@ -1,2 +1,58 @@
 import { userCollection } from "../../server";
 import type { I_User, I_Error, I_Success, I_Params, I_UserData, I_JwtVerification } from "../../../types";
+
+const get = async () => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			//
+		} catch (err) {
+			if (err instanceof Error) {
+				resolve({ status: false, error: err.message });
+			} else {
+				resolve({ status: false, error: "Unexpected Error." });
+			}
+		}
+	});
+};
+
+const create = async () => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			//
+		} catch (err) {
+			if (err instanceof Error) {
+				resolve({ status: false, error: err.message });
+			} else {
+				resolve({ status: false, error: "Unexpected Error." });
+			}
+		}
+	});
+};
+
+const update = async () => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			//
+		} catch (err) {
+			if (err instanceof Error) {
+				resolve({ status: false, error: err.message });
+			} else {
+				resolve({ status: false, error: "Unexpected Error." });
+			}
+		}
+	});
+};
+
+const remove = async () => {
+	return new Promise(async (resolve, reject) => {
+		try {
+			//
+		} catch (err) {
+			if (err instanceof Error) {
+				resolve({ status: false, error: err.message });
+			} else {
+				resolve({ status: false, error: "Unexpected Error." });
+			}
+		}
+	});
+};
